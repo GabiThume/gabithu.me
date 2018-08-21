f162606daccd6a7e50e4111f067fd30b17be48bf
Gabriela Thumé <gabithume@gmail.com>
2018-08-21 12:29:24 -0300

    Remove some old info

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  tagline: 'Full-stack Web Developer',
-  primaryColor: '#3498db', // Required
+  title: 'gabithume website', // Required
+  author: 'Gabriela Thumé', // Required
+  tagline: 'Software Engineer, Co-founder and Serial Entrepreneur',
+  primaryColor: '#000', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald'
+    website: 'https://gabithu.me',
+    github: 'https://github.com/gabithume',
+    twitter: 'https://twitter.com/gabithume',
+    linkedin: 'https://www.linkedin.com/in/gabithume/',
+    instagram: 'https://www.instagram.com/gabithume/'
   }
 };
--- src/components/HeaderImage/Wrapper.js
+++ src/components/HeaderImage/Wrapper.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  border: 5px solid #3598db;
-  border-radius: 50%;
+  border: 5px solid #000;
+  // border-radius: 50%;
   height: 150px;
   margin: 0 auto 15px auto;
   overflow: hidden;
@@ -10,7 +10,7 @@ const Wrapper = styled.div`
 
   img {
     padding: 5px;
-    border-radius: 50%;
+    // border-radius: 50%;
   }
 `;
 
